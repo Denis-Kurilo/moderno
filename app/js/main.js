7759b5ec7ed29633086dd91d75c48fb87c4c6e5a
Denis <kdb2015@mail.ru>
2019-11-29 17:14:51 +0300

    added aside search

--- app/js/main.js
+++ app/js/main.js
@@ -6,8 +6,15 @@ $(".rate-star").rateYo({
     readOnly: true
   });
 
-//mixitup
-var mixer = mixitup('.products__inner-box');
+//range-slider
+ $(".js-range-slider").ionRangeSlider({
+        type: "double",
+        min: 0,
+        max: 1000,
+        from: 0,
+        to: 600,
+        prefix: "$"
+    });
 
 $('.product-slider__inner').slick({
 	dots: true,
@@ -15,5 +22,8 @@ $('.product-slider__inner').slick({
 	slidesToShow: 4,
 	slidesToScroll: 4
 });
+
+//mixitup
+var mixer = mixitup('.products__inner-box');
 
 });
